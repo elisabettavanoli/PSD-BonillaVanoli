1a0fc24175c84f89c22b276b7e13421f831f91db
elisabettavanoli <elisabetta.vanoli@mail.polimi.it>
2025-07-22 14:31:39 +0200

    fixed workers and apis

--- services/collaborator/src/index.js
+++ services/collaborator/src/index.js
@@ -5,7 +5,7 @@ import { initialize } from "@oas-tools/core";
 import { bearerJwt } from "@oas-tools/auth/handlers";
 import fs from 'fs';
 
-const serverPort = 4000;
+const serverPort = 4002;
 const app = express();
 app.use(express.json({limit: '50mb'}));
 
